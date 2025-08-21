459d08b8a9605d05810ed9d38bbfec8da44a2910
vaidee <www.vaidees@gmail.com>
2025-08-21 16:41:44 +0800

    Create testconfig.py

---
+++ testconfig.py
@@ -0,0 +1,1 @@
+# Simulated secret key # FAKE AWS KEY for testing aws_access_key_id = "AKIAIOSFODNN7EXAMPLE" aws_secret_access_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY" DB_PASSWORD = "SuperSecret123!" secret = "SECRET-GUNEET-LEAK-12345" mediacorp_token="mediacorp-test-key-123"
